d33a443e7b2e5eb594f2e2ba97ae16eae3e6ac51
bernardoxm <bernardomelofacu@gmail.com>
2023-09-03 17:02:24 -0300

    Criado a lista de transacao

--- despesas_pessoais/lib/main.dart
+++ despesas_pessoais/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -10,6 +11,24 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+ final _transactions = [
+  Transaction(
+    id: 't1',
+    title: 'novo tenis de corrida', 
+    value: 310.76, 
+    date: DateTime.now(),),
+
+ Transaction(
+    id: 't2',
+    title: 'Conta de Luz', 
+    value: 211.30, 
+    date: DateTime.now(),),
+
+
+
+    ];
+ 
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
