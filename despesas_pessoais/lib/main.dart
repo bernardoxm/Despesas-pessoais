727a0659b905af47407ca5cd10e4e656ab311f4f
bernardoxm <bernardomelofacu@gmail.com>
2023-09-03 17:35:31 -0300

    add style

--- despesas_pessoais/lib/main.dart
+++ despesas_pessoais/lib/main.dart
@@ -11,24 +11,21 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
- final _transactions = [
-  Transaction(
-    id: 't1',
-    title: 'novo tenis de corrida', 
-    value: 310.76, 
-    date: DateTime.now(),),
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tenis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
- Transaction(
-    id: 't2',
-    title: 'Conta de Luz', 
-    value: 211.30, 
-    date: DateTime.now(),),
-
-
-
-    ];
- 
- 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,36 +37,40 @@ class MyHomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
-              child: Card(
-                child: Text('Grafico'),
-               color: Colors.blue,
-               elevation: 5,
-              ),
-              ),
-             Column(children:_transactions.map((tr)
-              {return Card(
-                                
-                child:Row(
-                  children: <Widget> [
-                  Container
-                  (child: Text
-                  (tr.value.toString()
+            child: Card(
+              child: Text('Grafico'),
+              color: Colors.blue,
+              elevation: 5,
+            ),
+          ),
+          Column(
+              children: _transactions.map((tr) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                    decoration: BoxDecoration(border: Border.all(color:Colors.purple,width: 2,)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(tr.value.toString(),
+                    style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.purple,),
+                    ),
                   ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(tr.title,
+                      style: TextStyle(fontSize: 16,fontWeight:FontWeight.bold),
+                      ),
+                      Text(tr.date.toString(),
+                      style: TextStyle(color:Colors.grey[800])) ,
+                      
+                    ],
                   ),
-                  Column(children:<Widget> [Text(tr.title),
-                  Text(tr.date.toString()),
-                  
-                  
-                  ],),
-                  ],
-
-                ) ,
-
-
-
-             );
-             }).toList()
-             )
+                ],
+              ),
+            );
+          }).toList())
         ],
       ),
     );
