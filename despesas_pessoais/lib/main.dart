ea54ae31e457932c60c6cf7cd75003af3ebfa4eb
bernardoxm <bernardomelofacu@gmail.com>
2023-09-03 16:53:38 -0300

    initialize

---
+++ despesas_pessoais/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+              child: Card(
+                child: Text('Grafico'),
+               color: Colors.blue,
+               elevation: 5,
+              ),
+              ),
+              Card(
+                child: Text('Lista de  Transacoes'),
+              ),
+        ],
+      ),
+    );
+  }
+}
