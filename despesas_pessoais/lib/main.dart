9d9c0102449ed3650e92c584138bc386ab6f08c6
bernardoxm <bernardomelofacu@gmail.com>
2023-09-03 22:07:55 -0300

    add button and style

--- despesas_pessoais/lib/main.dart
+++ despesas_pessoais/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
@@ -33,7 +34,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoais'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -49,29 +49,62 @@ class MyHomePage extends StatelessWidget {
               child: Row(
                 children: <Widget>[
                   Container(
-                    margin: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-                    decoration: BoxDecoration(border: Border.all(color:Colors.purple,width: 2,)),
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
                     padding: EdgeInsets.all(10),
-                    child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.purple,),
+                    child: Text(
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tr.title,
-                      style: TextStyle(fontSize: 16,fontWeight:FontWeight.bold),
-                      ),
                       Text(
-                        DateFormat('d MMM y').format(tr.date),
-                      style: TextStyle(color:Colors.grey[800])) ,
-                      
+                        tr.title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(DateFormat('d MMM y').format(tr.date),
+                          style: TextStyle(color: Colors.grey[800])),
                     ],
                   ),
                 ],
               ),
             );
-          }).toList())
+          }).toList()),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>
+                [TextField(decoration: InputDecoration(labelText: 'Titulo',)),
+                TextField(decoration: InputDecoration(labelText: 'Valor (R\$)'),),
+                 
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                   children: [
+                    
+                     ElevatedButton( child: Text('Nova Transacao'), onPressed: (){},
+                     
+                     style: ElevatedButton.styleFrom(primary: Colors.white, onPrimary:Colors.purple),
+                      ),
+                   ],
+                 ),
+                
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
