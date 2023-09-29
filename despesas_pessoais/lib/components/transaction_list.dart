a08cd50dde8e616498c7de767b3247184dbae274
bernardoxm <bernardomelofacu@gmail.com>
2023-09-28 22:28:19 -0300

    lest version2

--- despesas_pessoais/lib/components/transaction_list.dart
+++ despesas_pessoais/lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
+import 'package:despesas_pessoais/components/transaction_item.dart';
 import 'package:despesas_pessoais/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   List<Transaction> transactions;
@@ -38,50 +38,10 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              
-              
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text(
-                          'R\$${tr.value}',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text(
-                    DateFormat('d MMM y').format(tr.date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 400
-                      ? ElevatedButton.icon(
-                          onPressed: () => onRemove(tr.id),
-                          icon: const Icon(Icons.delete),
-                          label: const Text('Excluir'),
-                        style: ElevatedButton.styleFrom( primary: Theme.of(context).errorColor,
-                        
-                        
-                        ))
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () => onRemove(tr.id),
-                          color: Theme.of(context).copyWith().errorColor,
-                        ),
-                ),
+
+              return TransactionItem(
+                tr: tr,
+                onRemove: onRemove,
               );
             },
           );
